40085fdf1efcc8416b26563b0baf8c88db55b452
Sai Kiran Chetti <saikiranchetti18@gmail.com>
2021-07-16 13:06:29 +0530

    header and footer

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -4,6 +4,9 @@ import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+
 export class Main extends Component {
 
   constructor(props) {
@@ -21,13 +24,11 @@ export class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+         <Header />
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Footer />
+        
       </div>
     );
   }
