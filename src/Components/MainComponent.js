82ab51debe60c5510cebe5f38f81a27a2e04c6f3
Sai Kiran Chetti <saikiranchetti18@gmail.com>
2021-07-18 16:31:47 +0530

    controlled forms

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -8,6 +8,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -29,6 +30,14 @@ export class Main extends Component {
 
   render() {
 
+    const AboutUsPage = () => {
+      return(
+          <About 
+              leaders={this.state.leaders}
+          />
+      );
+  };
+
    
     const HomePage = () => {
       return(
@@ -39,13 +48,24 @@ export class Main extends Component {
           />
       );
     }
+
+
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
     return (
       <div>
          <Header />
          <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              <Route exact path='/contactus' component={Contact} />} />
+              <Route path='/menu/:dishId' component={DishWithId} />
+              <Route exact path='/contactus' component={Contact} />
+              <Route path='/aboutus' component={AboutUsPage}/>
               <Redirect to="/home" />
           </Switch>
         <Footer />
