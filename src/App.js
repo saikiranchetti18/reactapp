c83db8af5a0ed34052d1d2b91758d711f70953b6
Sai Kiran Chetti <saikiranchetti18@gmail.com>
2021-07-23 20:56:31 +0530

    combining reducers

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import React,{Component} from 'react';
 
 import Main from './Components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 export class App extends Component {
 
   render() {
     return (
+      <Provider store={store}>
       <BrowserRouter>
-      <div className="App">
-        <Main />
-      </div>
-    </BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+    </Provider>
     );
   }
 }
